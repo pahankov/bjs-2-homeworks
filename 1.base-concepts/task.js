632c7663d642d8e06ecbbbf7e323fd8476775111
Pahankov <Pahankov@mail.ru>
2026-02-09 19:41:29 +0300

    checking the first task

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,7 +1,17 @@
 "use strict"
 function solveEquation(a, b, c) {
   let arr = [];
+    const d = b * b - 4 * a * c;
+      if (d < 0) {
   
+  return arr;
+}
+  if (d === 0) {
+    arr.push(-b / (2 * a));
+    return arr;
+  }
+  arr.push((-b + Math.sqrt(d)) / (2 * a));
+  arr.push((-b - Math.sqrt(d)) / (2 * a));
   return arr;
 }
 
