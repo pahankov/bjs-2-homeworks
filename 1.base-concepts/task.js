a5811ac90c69497569c9b6c3be001502a2814cfd
Pahankov <Pahankov@mail.ru>
2026-02-09 19:49:36 +0300

    2

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,17 +1,22 @@
-"use strict"
+"use strict";
+
 function solveEquation(a, b, c) {
   let arr = [];
-    const d = b * b - 4 * a * c;
-      if (d < 0) {
+  const d = b * b - 4 * a * c;
   
-  return arr;
-}
+  if (d < 0) {
+    return arr;
+  }
+  
   if (d === 0) {
     arr.push(-b / (2 * a));
     return arr;
   }
-  arr.push((-b + Math.sqrt(d)) / (2 * a));
-  arr.push((-b - Math.sqrt(d)) / (2 * a));
+  
+  arr.push(
+    (-b + Math.sqrt(d)) / (2 * a),
+    (-b - Math.sqrt(d)) / (2 * a)
+  );
   return arr;
 }
 
