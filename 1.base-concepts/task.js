e68d06bb3dc7c71c8e95194cf6bd2885f5aaf433
Pahankov <Pahankov@mail.ru>
2026-02-09 21:33:51 +0300

    checking the second task

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,25 +1,50 @@
+///Задание 1
+
 "use strict";
 
 function solveEquation(a, b, c) {
   let arr = [];
-  const d = b * b - 4 * a * c;
+  const discriminant = b ** 2 - 4 * a * c;
   
-  if (d < 0) {
+  if (discriminant < 0) {
     return arr;
   }
   
-  if (d === 0) {
-    arr.push(-b / (2 * a));
+  if (discriminant === 0) {
+    const root = -b / (2 * a);
+    arr.push(root);
     return arr;
   }
   
-  arr.push(
-    (-b + Math.sqrt(d)) / (2 * a),
-    (-b - Math.sqrt(d)) / (2 * a)
-  );
+  const root1 = (-b + Math.sqrt(discriminant)) / (2 * a);
+  const root2 = (-b - Math.sqrt(discriminant)) / (2 * a);
+  arr.push(root1, root2);
   return arr;
 }
 
+///Задание 2
+
+
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
+  percent = parseFloat(percent);
+  contribution = parseFloat(contribution);
+  amount = parseFloat(amount);
+  countMonths = parseFloat(countMonths);
   
+  if (isNaN(percent) || isNaN(contribution) || 
+      isNaN(amount) || isNaN(countMonths)) {
+    return false;
+  }
+  
+  const P = percent / 100 / 12;
+  
+  const S = amount - contribution;
+  
+  const n = countMonths;
+  
+  const payment = S * (P + (P / (Math.pow(1 + P, n) - 1)));
+  
+  const total = payment * n;
+  
+  return Math.round(total * 100) / 100;
 }
