7672eeac905ea9457a5d1c3d43c14e3e6e356deb
Pahankov <Pahankov@mail.ru>
2026-02-11 19:00:15 +0300

    first task on functions

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,6 +1,32 @@
+///Задание 1
 function getArrayParams(...arr) {
 
-  return { min: min, max: max, avg: avg };
+  if (arr.length === 0) {          
+    return { min: 0, max: 0, avg: 0 }; 
+  }
+  
+  let min = arr[0]; 
+  let max = arr[0];  
+  let sum = 0;      
+  
+  for (let i = 0; i < arr.length; i++) { 
+    
+    if (arr[i] < min) {     
+      min = arr[i];         
+    }
+    
+    if (arr[i] > max) {     
+      max = arr[i];         
+    }
+    
+    sum = sum + arr[i];    
+  }
+  
+  let avg = sum / arr.length;  
+  
+  avg = Number(avg.toFixed(2));  
+  
+  return { min: min, max: max, avg: avg };  
 }
 
 function summElementsWorker(...arr) {
