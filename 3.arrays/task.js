7d4f8502cbb242de3fceac71eb4930d1112237a8
Pahankov <Pahankov@mail.ru>
2026-02-16 19:42:43 +0300

    tasks array

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,23 @@
+///Задание 1
 function compareArrays(arr1, arr2) {
-  
+
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+
+  return arr1.every((item, index) => item === arr2[index]);
 }
 
+///Задание 2
 function getUsersNamesInAgeRange(users, gender) {
   
+  const filteredUsers = users.filter(user => user.gender === gender);  
+
+  if (filteredUsers.length === 0) {
+    return 0;  
+  }
+
+  const ages = filteredUsers.map(user => user.age);
+  const sum = ages.reduce((acc, age) => acc + age, 0);
+  return sum / filteredUsers.length;      
 }
