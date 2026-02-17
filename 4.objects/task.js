05b9b2a334d39bdb5825cf739ce493edd3a5a7c1
Pahankov <Pahankov@mail.ru>
2026-02-17 20:05:20 +0300

    task objects

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,51 @@
+///Задание 1
 function Student(name, gender, age) {
-  
+
+  this.name = name;      
+  this.gender = gender;    
+  this.age = age;        
+  this.marks = [];    
 }
 
+///Задание 2
 Student.prototype.setSubject = function (subjectName) {
-  
+
+  this.subject = subjectName;     
 }
 
+///Задание 3
 Student.prototype.addMarks = function (...marks) {
   
-}
+  if (!this.marks) {
+    return;
+  }
 
-Student.prototype.getAverage = function () {
+  for (let i = 0; i < marksToAdd.length; i++) {
+    this.marks.push(marksToAdd[i]);
+  }  
   
 }
 
+///Задание 4
+Student.prototype.getAverage = function () {
+
+  if (!this.marks || this.marks.length === 0) {
+    return 0;
+  }
+  
+  let sum = 0;
+  for (let i = 0; i < this.marks.length; i++) {
+    sum += this.marks[i];
+  }
+  
+  return sum / this.marks.length;  
+}
+
+///Задание 5
 Student.prototype.exclude = function (reason) {
   
+  delete this.subject;
+  delete this.marks;
+
+  this.excluded = reason;  
 }
