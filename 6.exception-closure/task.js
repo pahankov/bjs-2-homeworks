c39595e5b5a21c4f1f0bba0d97a1295e8f048c94
Pahankov <Pahankov@mail.ru>
2026-02-27 18:08:11 +0300

    first task exclusion

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,1 +1,19 @@
-﻿
+﻿///Задание 1
+function parseCount(pars) {
+  const parsedValue = Number.parseFloat(pars);
+    if (isNaN(parsedValue)) {
+    throw new Error("Невалидное значение");
+  }
+  
+  return parsedValue;
+}
+
+function validateCount(pars) {
+  try {
+    return parseCount(pars);
+  } 
+  catch (error) {
+    return error;
+  }
+}
+
