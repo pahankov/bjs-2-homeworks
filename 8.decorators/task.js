9208f6c6e65a373329756b7aa9918b162cb8a5df
Pahankov <Pahankov@mail.ru>
2026-02-28 19:48:58 +0300

    first task is decorators

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,6 +1,26 @@
 //Задача № 1
 function cachingDecoratorNew(func) {
-  
+  let cache = [];
+
+  function wrapper(...args) {
+    const hash = md5(args);
+    let objectInCache = cache.find((item) => item.hash === hash);
+    if (objectInCache) {
+      console.log("Из кеша: " + objectInCache.value);
+      return "Из кеша: " + objectInCache.value;
+    }
+    let result = func(...args);
+    cache.push({
+      hash: hash,
+      value: result
+    });
+    if (cache.length > 5) {
+      cache.shift();
+    }
+    console.log("Вычисляем: " + result);
+    return "Вычисляем: " + result;  
+  }
+  return wrapper;
 }
 
 //Задача № 2
